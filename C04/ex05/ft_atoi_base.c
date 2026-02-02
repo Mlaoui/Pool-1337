0a11bf4f0a18b01aa0965c2fd8ecb8db951e8146
Unknown <mohamed.laoui@gmail.com>
2026-02-02 16:17:30 +0100

    ft_atoi_base added

--- C04/ex05/ft_atoi_base.c
+++ C04/ex05/ft_atoi_base.c
@@ -1,1 +1,69 @@
-#include <unistd.h>
+#include <stdio.h>
+
+int     checkbase(char *base)
+{
+    int i;
+    int j;
+    if (base[0] == '\0' || base[1] == '\0')
+        return 0;
+    i = 0;
+    while (base[i])
+    {
+        if (base[i] == ' ' || (base[i] >= 9 && base[i] <= 13) || base[i] == '-' || base[i] == '+')
+            return 0;
+        j = i + 1;
+        while(base[j])
+        {
+            if (base[i] == base[j])
+                return 0;
+            j++;
+        }
+        i++;
+    }
+    return i;
+}
+
+int     value(char c,char *base)
+{
+    int i = 0;
+    while (base[i])
+    {
+        if (base[i] == c)
+            return i;
+        i++;
+    }
+    return -1;
+}
+
+int ft_atoi_base(char *str,char *base)
+{
+    int len_of_base = checkbase(base);
+    if (len_of_base == 0)
+        return 0;
+    int result = 0;
+    int sign = 1;
+    int i = 0;
+    while (str[i] == ' ' || (str[i] >= 9 && str[i] <= 13))
+            i++;
+    while (str[i] == '-' || str[i] == '+')
+    {
+        if (str[i] == '-')
+            sign *= -1;
+        i++;
+    }
+    while (value(str[i],base) != -1)
+    {
+
+        result = result * len_of_base + value(str[i],base);
+        i++;
+    }
+    return result * sign;
+}
+
+int main()
+{
+    char *base = "01";
+    char *str = "  -+-101avs";
+    printf("expected returned value is 5 %d",ft_atoi_base(str,base));
+    return 0;
+}
