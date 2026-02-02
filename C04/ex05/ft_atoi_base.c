2f66633d64e5f12f6e305b4a62a31f7b596da52a
Mohamed Laoui <mohamed.laoui@hotmail.com>
2026-02-02 17:53:23 +0100

    norm passed

--- C04/ex05/ft_atoi_base.c
+++ C04/ex05/ft_atoi_base.c
@@ -1,69 +1,93 @@
 #include <stdio.h>
 
-int     checkbase(char *base)
+int	check_base(char *base)
 {
-    int i;
-    int j;
-    if (base[0] == '\0' || base[1] == '\0')
-        return 0;
-    i = 0;
-    while (base[i])
-    {
-        if (base[i] == ' ' || (base[i] >= 9 && base[i] <= 13) || base[i] == '-' || base[i] == '+')
-            return 0;
-        j = i + 1;
-        while(base[j])
-        {
-            if (base[i] == base[j])
-                return 0;
-            j++;
-        }
-        i++;
-    }
-    return i;
+	int	i;
+	int	j;
+
+	if (!base || base[0] == '\0' || base[1] == '\0')
+		return (0);
+	i = 0;
+	while (base[i])
+	{
+		if (base[i] == ' ' || (base[i] >= 9 && base[i] <= 13)
+			|| base[i] == '-' || base[i] == '+')
+			return (0);
+		j = i + 1;
+		while (base[j])
+		{
+			if (base[i] == base[j])
+				return (0);
+			j++;
+		}
+		i++;
+	}
+	return (i);
 }
 
-int     value(char c,char *base)
+int	value(char c, char *base)
 {
-    int i = 0;
-    while (base[i])
-    {
-        if (base[i] == c)
-            return i;
-        i++;
-    }
-    return -1;
+	int	i;
+
+	i = 0;
+	while (base[i])
+	{
+		if (base[i] == c)
+			return (i);
+		i++;
+	}
+	return (-1);
 }
 
-int ft_atoi_base(char *str,char *base)
+int	skip_spaces_signs(char *str, int *sign)
 {
-    int len_of_base = checkbase(base);
-    if (len_of_base == 0)
-        return 0;
-    int result = 0;
-    int sign = 1;
-    int i = 0;
-    while (str[i] == ' ' || (str[i] >= 9 && str[i] <= 13))
-            i++;
-    while (str[i] == '-' || str[i] == '+')
-    {
-        if (str[i] == '-')
-            sign *= -1;
-        i++;
-    }
-    int v = value(str[i],base);
-    while (v != -1)
-    {
-        result = result * len_of_base + v;
-        i++;
-    }
-    return result * sign;
+	int	i;
+
+	i = 0;
+	while (str[i] == ' ' || (str[i] >= 9 && str[i] <= 13))
+		i++;
+	while (str[i] == '-' || str[i] == '+')
+	{
+		if (str[i] == '-')
+			*sign *= -1;
+		i++;
+	}
+	return (i);
 }
 
-int main()
+int	ft_atoi_base(char *str, char *base)
 {
-    char *base = "01";
-    char *str = "  -+-101avs";
-    printf("expected returned value is 5 %d",ft_atoi_base(str,base));
-    return 0;
+	int	result;
+	int	sign;
+	int	i;
+	int	v;
+	int	len_of_base;
+
+	if (!str || !base)
+		return (0);
+	result = 0;
+	sign = 1;
+	len_of_base = check_base(base);
+	if (len_of_base == 0)
+		return (0);
+	i = skip_spaces_signs(str, &sign);
+	v = value(str[i], base);
+	while (v != -1)
+	{
+		result = result * len_of_base + v;
+		i++;
+		v = value(str[i], base);
+	}
+	return (result * sign);
+}
+
+int	main(void)
+{
+	char	*base;
+	char	*str;
+
+	base = "01";
+	str = "  -+-101avs";
+	printf("expected returned value is 5 %d\n", ft_atoi_base(str, base));
+	return (0);
 }
