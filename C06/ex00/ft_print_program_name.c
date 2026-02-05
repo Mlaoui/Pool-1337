8ca1ab76e59e35339123096dcde159d15ea5b4ff
Unknown <mohamed.laoui@gmail.com>
2026-02-05 13:48:31 +0100

    ex00 added

---
+++ C06/ex00/ft_print_program_name.c
@@ -0,0 +1,14 @@
+#include <unistd.h>
+
+int	main(int ac, char **av)
+{
+	int	len;
+
+	ac += 0;
+	len = 0;
+	while (av[0][len])
+		len++;
+	write(1, av[0], len);
+	write(1, "\n", 1);
+	return (0);
+}
